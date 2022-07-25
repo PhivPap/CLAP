e1c0fc46988ad06a04bf4aeca9df59f3cfa6a905
Phivos Papapanagiotakis <phoivos@windowslive.com>
2022-07-25 18:01:55 +0300

    Improve exception, add double parsing, improve demo & add comments

--- CLAP/CLAP.cpp
+++ CLAP/CLAP.cpp
@@ -3,60 +3,94 @@
 
 Handler::Handler(bool required, unsigned bindings, HandlerType type) : required(required), bindings(bindings), type(type) {}
 Handler::~Handler() {}
-void Handler::void_arg_caller(void) const { abort(); }									// why am I here? 
-void Handler::single_arg_caller(const std::string& str) const { abort(); }				// why am I here? 
-void Handler::multi_arg_caller(const std::vector<std::string>& vec) const { abort(); }	// why am I here? 
+void Handler::void_arg_caller(void) const { abort(); }								// why am I here? 
+void Handler::single_arg_caller(std::string& str) const { abort(); }				// why am I here? 
+void Handler::multi_arg_caller(std::vector<std::string>& vec) const { abort(); }	// why am I here? 
 
-VoidHandler::VoidHandler(bool required, unsigned bindings, HandlerType type, std::function<void(void)> callback)
-	: Handler(required, bindings, type), callback(callback) {}
+VoidHandler::VoidHandler(bool required, HandlerType type, const std::function<void(void)>& callback)
+	: Handler(required, 0, type), callback(callback) {}
 VoidHandler::~VoidHandler(void) {}
 void VoidHandler::void_arg_caller(void) const { callback(); }
 
-SingleStrHandler::SingleStrHandler(bool required, unsigned bindings, HandlerType type, std::function<void(const std::string&)> callback)
-	: Handler(required, bindings, type), callback(callback) {}
+SingleStrHandler::SingleStrHandler(bool required, HandlerType type, const std::function<void(std::string&)>& callback)
+	: Handler(required, 1, type), callback(callback) {}
 SingleStrHandler::~SingleStrHandler(void) {}
-void SingleStrHandler::single_arg_caller(const std::string& str) const { callback(str); }
+void SingleStrHandler::single_arg_caller(std::string& str) const { callback(str); }
 
-MultiStrHandler::MultiStrHandler(bool required, unsigned bindings, HandlerType type, std::function<void(const std::vector<std::string>&)> callback)
+MultiStrHandler::MultiStrHandler(bool required, unsigned bindings, HandlerType type, const std::function<void(std::vector<std::string>&)>& callback)
 	: Handler(required, bindings, type), callback(callback) {}
 MultiStrHandler::~MultiStrHandler(void) {}
-void MultiStrHandler::multi_arg_caller(const std::vector<std::string>& vec) const { callback(vec); }
+void MultiStrHandler::multi_arg_caller(std::vector<std::string>& vec) const { callback(vec); }
 
-SingleIntHandler::SingleIntHandler(bool required, unsigned bindings, HandlerType type, std::function<void(int64_t)> callback)
-	: Handler(required, bindings, type), callback(callback) {}
+SingleIntHandler::SingleIntHandler(bool required, HandlerType type, const std::function<void(int64_t)>& callback)
+	: Handler(required, 1, type), callback(callback) {}
 SingleIntHandler::~SingleIntHandler(void) {}
-void SingleIntHandler::single_arg_caller(const std::string& str) const {
+void SingleIntHandler::single_arg_caller(std::string& str) const {
 	try {
 		callback(std::stoll(str));
 	}
-	catch (const std::invalid_argument& _) {
+	catch (const std::invalid_argument& ) {
 		throw "Impossible conversion of '" + str + "' to int64_t.";
 	}
-	catch (const std::out_of_range& _) {
+	catch (const std::out_of_range& ) {
 		throw "Impossible conversion of '" + str + "' to int64_t (out of range).";
 	}
 }
 
-MultiIntHandler::MultiIntHandler(bool required, unsigned bindings, HandlerType type, std::function<void(const std::vector<int64_t>&)> callback)
+MultiIntHandler::MultiIntHandler(bool required, unsigned bindings, HandlerType type, const std::function<void(std::vector<int64_t>&)>& callback)
 	: Handler(required, bindings, type), callback(callback) {}
 MultiIntHandler::~MultiIntHandler(void) {}
-void MultiIntHandler::multi_arg_caller(const std::vector<std::string>& vec) const {
+void MultiIntHandler::multi_arg_caller(std::vector<std::string>& vec) const {
 	std::vector<int64_t> int_vec;
 	int_vec.reserve(vec.size());
 	for (const auto& str : vec) {
 		try {
 			int_vec.push_back(std::stoll(str));
 		}
-		catch (const std::invalid_argument& e) {
+		catch (const std::invalid_argument& ) {
 			throw "Impossible conversion of '" + str + "' to int64_t.";
 		}
-		catch (const std::out_of_range& e) {
+		catch (const std::out_of_range& ) {
 			throw "Impossible conversion of '" + str + "' to int64_t (out of range).";
 		}
 	}
 	callback(int_vec);
 }
 
+SingleDblHandler::SingleDblHandler(bool required, HandlerType type, const std::function<void(double)>& callback) 
+	: Handler(required, 1, type), callback(callback) {}
+SingleDblHandler::~SingleDblHandler(void) {}
+void SingleDblHandler::single_arg_caller(std::string& str) const {
+	try {
+		callback(std::stod(str));
+	}
+	catch (const std::invalid_argument&) {
+		throw "Impossible conversion of '" + str + "' to double.";
+	}
+	catch (const std::out_of_range&) {
+		throw "Impossible conversion of '" + str + "' to double (out of range).";
+	}
+}
+
+MultiDblHandler::MultiDblHandler(bool required, unsigned bindings, HandlerType type, const std::function<void(std::vector<double>&)>& callback)
+	: Handler(required, bindings, type), callback(callback) {}
+MultiDblHandler::~MultiDblHandler(void) {}
+void MultiDblHandler::multi_arg_caller(std::vector<std::string>& vec) const {
+	std::vector<double> dbl_vec;
+	dbl_vec.reserve(vec.size());
+	for (const auto& str : vec) {
+		try {
+			dbl_vec.push_back(std::stod(str));
+		}
+		catch (const std::invalid_argument&) {
+			throw "Impossible conversion of '" + str + "' to int64_t.";
+		}
+		catch (const std::out_of_range&) {
+			throw "Impossible conversion of '" + str + "' to int64_t (out of range).";
+		}
+	}
+	callback(dbl_vec);
+}
 
 
 CLAP::CLAP(void) {}
@@ -65,26 +99,33 @@ CLAP::~CLAP(void) {
 		delete _handler;
 }
 
-void CLAP::register_arg(const std::string& name, std::function<void(void)> callback, bool required) {
-	add_handler(name, new VoidHandler(required, 0, Handler::HandlerType::None, callback));
+void CLAP::register_arg(const std::string& name, const std::function<void(void)>& callback, bool required) {
+	add_handler(name, new VoidHandler(required, Handler::HandlerType::None, callback));
 }
 
-void CLAP::register_bound_arg(const std::string& name, std::function<void(const std::string&)> callback, bool required) {
-	add_handler(name, new SingleStrHandler(required, 1, Handler::HandlerType::Single, callback));
+void CLAP::register_bound_arg(const std::string& name, const std::function<void(std::string&)>& callback, bool required) {
+	add_handler(name, new SingleStrHandler(required, Handler::HandlerType::Single, callback));
 }
 
-void CLAP::register_multibound_arg(const std::string& name, std::function<void(const std::vector<std::string>&)> callback, unsigned bindings, bool required) {
+void CLAP::register_bound_arg(const std::string& name, const std::function<void(int64_t)>& callback, bool required) {
+	add_handler(name, new SingleIntHandler(required, Handler::HandlerType::Single, callback));
+}
+
+void CLAP::register_bound_arg(const std::string& name, const std::function<void(double)>& callback, bool required) {
+	add_handler(name, new SingleDblHandler(required, Handler::HandlerType::Single, callback));
+}
+
+void CLAP::register_multibound_arg(const std::string& name, const std::function<void(std::vector<std::string>&)>& callback, unsigned bindings, bool required) {
 	add_handler(name, new MultiStrHandler(required, bindings, Handler::HandlerType::Multi, callback));
 }
 
-void CLAP::register_bound_arg(const std::string& name, std::function<void(int64_t)> callback, bool required) {
-	add_handler(name, new SingleIntHandler(required, 1, Handler::HandlerType::Single, callback));
-}
-
-void CLAP::register_multibound_arg(const std::string& name, std::function<void(const std::vector<int64_t>&)> callback, unsigned bindings, bool required) {
+void CLAP::register_multibound_arg(const std::string& name, const std::function<void(std::vector<int64_t>&)>& callback, unsigned bindings, bool required) {
 	add_handler(name, new MultiIntHandler(required, bindings, Handler::HandlerType::Multi, callback));
 }
 
+void CLAP::register_multibound_arg(const std::string& name, std::function<void(std::vector<double>&)> callback, unsigned bindings, bool required) {
+	add_handler(name, new MultiDblHandler(required, bindings, Handler::HandlerType::Multi, callback));
+}
 
 
 
@@ -93,7 +134,7 @@ void CLAP::check_missing_or_duplicates(int argc, const char** argv) {
 	for (const auto& [name, _handler] : arg_handlers) {
 		const Handler& handler = *_handler;
 		bool parsed = false;
-		for (unsigned i = 0; i < argc; i++) {
+		for (int i = 0; i < argc; i++) {
 			if (name == argv[i]) {
 				if (parsed)
 					throw "CLAP: Command line arguments contain duplicate flag '" + name + "'.";
@@ -110,7 +151,7 @@ void CLAP::parse(int argc, const char** argv) {
 	for (const auto& [name, _handler] : arg_handlers) {
 		const Handler& handler = *_handler;
 		bool parsed = false;
-		for (unsigned i = 0; i < argc; i++) {
+		for (int i = 0; i < argc; i++) {
 			if (name == argv[i]) {
 				switch (handler.type) {
 
@@ -122,7 +163,8 @@ void CLAP::parse(int argc, const char** argv) {
 					if (i + 1 >= argc)
 						throw "CLAP: Missing binding for argument '" + name + "'.";
 					try {
-						handler.single_arg_caller(argv[i + 1]);
+						std::string s = argv[i + 1];
+						handler.single_arg_caller(s);
 					}
 					catch (std::string& ex) {
 						throw "CLAP: Parsing of argument '" + name + "' produced exception:\n\t" + ex;
@@ -133,17 +175,17 @@ void CLAP::parse(int argc, const char** argv) {
 					std::vector<std::string> bindings;
 					const auto end = arg_handlers.end();
 					if (handler.bindings == 0) {
-						const unsigned threshold = argc - 1;
-						for (unsigned j = i + 1; j <= threshold && arg_handlers.find(argv[j]) == end; j++)
+						const int threshold = argc - 1;
+						for (int j = i + 1; j <= threshold && arg_handlers.find(argv[j]) == end; j++)
 							bindings.push_back(argv[j]);
 					}
 					else {
-						const unsigned threshold = i + handler.bindings;
+						const int threshold = i + handler.bindings;
 						if (threshold >= argc)
 							throw "CLAP: Missing bindings for argument '" + name + "'.";
 						bindings.reserve(handler.bindings);
 						unsigned binding_counter = 0;
-						for (unsigned j = i + 1; j <= threshold; j++) {
+						for (int j = i + 1; j <= threshold; j++) {
 							if (arg_handlers.find(argv[j]) != end)
 								throw "CLAP: Argument '" + name + "' expected " + std::to_string(handler.bindings) + " bindings but got " + std::to_string(binding_counter) + ".";
 							bindings.push_back(argv[j]);
--- CLAP/CLAP.h
+++ CLAP/CLAP.h
@@ -18,89 +18,124 @@ public:
 	Handler(bool required, unsigned bindings, HandlerType type);
 	virtual ~Handler(void);
 	virtual void void_arg_caller(void) const;
-	virtual void single_arg_caller(const std::string& str) const;
-	virtual void multi_arg_caller(const std::vector<std::string>& vec) const;
+	virtual void single_arg_caller(std::string& str) const;
+	virtual void multi_arg_caller(std::vector<std::string>& vec) const;
 };
 
 class VoidHandler : public Handler {
 private:
 	const std::function<void(void)> callback;
 public:
-	VoidHandler(bool required, unsigned bindings, HandlerType type, std::function<void(void)> callback);
+	VoidHandler(bool required, HandlerType type, const std::function<void(void)>& callback);
 	virtual ~VoidHandler();
 	virtual void void_arg_caller(void) const;
 };
 
 class SingleStrHandler : public Handler {
 private:
-	const std::function<void(const std::string&)> callback;
+	const std::function<void(std::string&)> callback;
 public:
-	SingleStrHandler(bool required, unsigned bindings, HandlerType type, std::function<void(const std::string&)> callback);
+	SingleStrHandler(bool required, HandlerType type, const std::function<void(std::string&)>& callback);
 	virtual ~SingleStrHandler();
-	virtual void single_arg_caller(const std::string& str) const;
+	virtual void single_arg_caller(std::string& str) const;
 };
 
 class MultiStrHandler : public Handler {
 private:
-	const std::function<void(const std::vector<std::string>&)> callback;
+	const std::function<void(std::vector<std::string>&)> callback;
 public:
-	MultiStrHandler(bool required, unsigned bindings, HandlerType type, std::function<void(const std::vector<std::string>&)> callback);
+	MultiStrHandler(bool required, unsigned bindings, HandlerType type, const std::function<void(std::vector<std::string>&)>& callback);
 	virtual ~MultiStrHandler();
-	virtual void multi_arg_caller(const std::vector<std::string>& vec) const;
+	virtual void multi_arg_caller(std::vector<std::string>& vec) const;
 };
 
 class SingleIntHandler : public Handler {
 private:
 	const std::function<void(int64_t)> callback;
 public:
-	SingleIntHandler(bool required, unsigned bindings, HandlerType type, std::function<void(int64_t)> callback);
+	SingleIntHandler(bool required, HandlerType type, const std::function<void(int64_t)>& callback);
 	virtual ~SingleIntHandler();
-	virtual void single_arg_caller(const std::string& str) const;
+	virtual void single_arg_caller(std::string& str) const;
 };
 
 class MultiIntHandler : public Handler {
 private:
-	const std::function<void(const std::vector<int64_t>&)> callback;
+	const std::function<void(std::vector<int64_t>&)> callback;
 public:
-	MultiIntHandler(bool required, unsigned bindings, HandlerType type, std::function<void(const std::vector<int64_t>&)> callback);
+	MultiIntHandler(bool required, unsigned bindings, HandlerType type, const std::function<void(std::vector<int64_t>&)>& callback);
 	virtual ~MultiIntHandler();
-	virtual void multi_arg_caller(const std::vector<std::string>& vec) const;
+	virtual void multi_arg_caller(std::vector<std::string>& vec) const;
 };
 
+class SingleDblHandler : public Handler {
+private:
+	const std::function<void(double)> callback;
+public:
+	SingleDblHandler(bool required, HandlerType type, const std::function<void(double)>& callback);
+	virtual ~SingleDblHandler(void);
+	virtual void single_arg_caller(std::string& str) const;
+};
+
+class MultiDblHandler : public Handler {
+private:
+	const std::function<void(std::vector<double>&)> callback;
+public:
+	MultiDblHandler(bool required, unsigned bindings, HandlerType type, const std::function<void(std::vector<double>&)>& callback);
+	virtual ~MultiDblHandler(void);
+	virtual void multi_arg_caller(std::vector<std::string>& vec) const;
+};
 
 class CLAP {
 public:
+	using FunSingleInt = std::function<void(int64_t)>;
+	using FunMultiInt = std::function<void(std::vector<int64_t>&)>;
+	using FunSingleDbl = std::function<void(double)>;
+	using FunMultiDbl = std::function<void(std::vector<double>&)>;
+
 	CLAP(void);
 	virtual ~CLAP(void);
 
 	void register_arg(
 		const std::string& name,
-		std::function<void(void)> callback,
+		const std::function<void(void)>& callback,
 		bool required = false
 	);
 
 	void register_bound_arg(
 		const std::string& name,
-		std::function<void(const std::string&)> callback,
+		const std::function<void(std::string&)>& callback,
 		bool required = false
 	);
 
 	void register_bound_arg(
 		const std::string& name,
-		std::function<void(int64_t)> callback,
+		const std::function<void(int64_t)>& callback,
+		bool required = false
+	);
+
+	void register_bound_arg(
+		const std::string& name,
+		const std::function<void(double)>& callback,
 		bool required = false
 	);
 
 	void register_multibound_arg(
 		const std::string& name,
-		std::function<void(const std::vector<std::string>&)> callback,
+		const std::function<void(std::vector<std::string>&)>& callback,
 		unsigned bindings,
 		bool required = false
 	);
 
 	void register_multibound_arg(
 		const std::string& name,
-		std::function<void(const std::vector<int64_t>&)> callback,
+		const std::function<void(std::vector<int64_t>&)>& callback,
+		unsigned bindings,
+		bool required = false
+	);
+
+	void register_multibound_arg(
+		const std::string& name,
+		std::function<void(std::vector<double>&)> callback,
 		unsigned bindings,
 		bool required = false
 	);
